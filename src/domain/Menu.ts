9be5db3a1569e413ffa7c5284ffe22a4fc2ad73b
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-07-04 17:11:33 +0600

    feat: Menu improvment

--- src/domain/Menu.ts
+++ src/domain/Menu.ts
@@ -7,7 +7,18 @@ export class Menu {
     this.foodItems.push(foodItem);
   }
 
-  getFoodItems(): FoodItem[] {
-    return this.foodItems;
+  getFoodItems(): readonly FoodItem[] {
+    return [...this.foodItems];
+  }
+
+  removeFoodItem(foodItem: FoodItem): void {
+    const index = this.foodItems.findIndex((item) => item.id === foodItem.id);
+    if (index !== -1) {
+      this.foodItems.splice(index, 1);
+    }
+  }
+
+  findFoodItemBy(id: number): FoodItem | undefined {
+    return this.foodItems.find((item) => item.id === id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Dash dine");
+import { FoodItem } from "./domain/FoodItem";
+import { Menu } from "./domain/Menu";
+import { Restaurent } from "./domain/Restaurent";
+
+const burger = new FoodItem(1, "Burger", 5.99);
+const pizza = new FoodItem(2, "Pizza", 8.99);
+
+const menu = new Menu([burger, pizza]);
+
+const restaurent = new Restaurent(1, "My Restaurent", menu);
+
+console.log(`Restaurent: ${restaurent.name}`);
