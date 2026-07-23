48ba9b7a62d1a5009ac817d65281cd6f235586a1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-07-23 18:05:19 +0600

    feat: factory class

---
+++ src/payment/PaymentFactory.ts
@@ -0,0 +1,19 @@
+import { ApplePayPayment } from "./ApplePayPayment";
+import { CreditCardPayment } from "./CreditCardPayment";
+import { PaymentMethod } from "./PayementMethod";
+import { PaypalPayment } from "./PaypalPayment";
+
+class PaymentFactory {
+  static create(type: string): PaymentMethod {
+    switch (type) {
+      case "credit":
+        return new CreditCardPayment();
+      case "paypal":
+        return new PaypalPayment();
+      case "apple":
+        return new ApplePayPayment();
+      default:
+        throw new Error(`Payment method ${type} not supported`);
+    }
+  }
+}
